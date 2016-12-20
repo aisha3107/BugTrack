20b8b19fb72dda5f31267b6237623038b36f0cea
Муратова Айгерим Асхаткызы <ai.muratova@mail.ru>
2016-12-20 17:23:16 +0600

    Bll for project tasks and projects added

---
+++ BugTrack/BugTrack/BLL/ProjectsBll.cs
@@ -0,0 +1,23 @@
+﻿using BugTrack.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrack.BLL
+{
+    public class ProjectsBll
+    {
+        BugTrackEntities db;
+
+        public ProjectsBll()
+        {
+            db = new BugTrackEntities();
+        }
+
+        public List<Projects> GetProjects()
+        {
+            return db.Projects.ToList();
+        }
+    }
+}
