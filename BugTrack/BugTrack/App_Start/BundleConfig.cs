fedf4297acd10b4a309c4fc580716c9a8b5ccfea
Муратова Айгерим Асхаткызы <ai.muratova@mail.ru>
2016-12-27 17:29:10 +0600

    Start page example

--- BugTrack/BugTrack/App_Start/BundleConfig.cs
+++ BugTrack/BugTrack/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace BugTrack
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -26,6 +29,9 @@ namespace BugTrack
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/home").Include(
+                      "~/Content/HomePage.css"));
         }
     }
 }
